8fcfca6d3a2283d6932b9bbf33f1476c56f96240
Denis Glazkov <denis@begom.com>
2015-08-04 15:09:39 +0300

    modify components, use API.getUserInfo, fix css

--- lib/components/UserInfo.jsx
+++ lib/components/UserInfo.jsx
@@ -9,26 +9,31 @@ var UserInfo = $class({
     getInitialState: function() {
         var user = this.props.user;
         var cert = baseAPI.parseCertificate(user.cert? user.cert : user);
-        if(cert) {
-            if(cert.isAnonymous()) {
-                return {
-                    isAnonymous: true,
-                    nickname: "anonymous-"+parseInt(cert.pub.substr(0,8), 16).toString(36).toUpperCase(),
-                    ico: STD_ICONS_ANON[parseInt(cert.pub.substr(-10), 16) % STD_ICONS_ANON.length],
-                    color: "#" + cert.pub.substr(-6)
-                };
-            }
-            baseAPI.getAuthorInfo(cert.toString(), function(err, info){
-                setTimeout(this.setState.bind(this, err? {error: "ERROR: "+err} : info));
-            }.bind(this));
+        if(!cert) return {};
+        if(cert.isAnonymous()) {
+            return {
+                isAnonymous: true,
+                name: "anonymous-"+parseInt(cert.pub.substr(0,8), 16).toString(36).toUpperCase(),
+                ico: STD_ICONS_ANON[parseInt(cert.pub.substr(-10), 16) % STD_ICONS_ANON.length],
+                color: "#" + cert.pub.substr(-6)
+            };
         }
+        baseAPI.getCertificateInfo(cert.toString(), function(err, info){
+            info.domain = info.domain || info.name + ".base.network";
+            setTimeout(this.setState.bind(this, err? {error: "ERROR: "+err} : info));
+        }.bind(this));
+
+        baseAPI.getUserInfo(cert.toString(), function(err, info){
+            setTimeout(this.setState.bind(this, err? {error: "ERROR: "+err} : {userInfo: info}));
+        }.bind(this));
         return {
+            ico: STD_ICONS_ANON[parseInt(cert.pub.substr(-10), 16) % STD_ICONS_ANON.length],
             color: "#" + cert.pub.substr(-6)
         }
     },
 
     toString: function() {
-        return this.state[this.props.type || "nickname"]
+        return this.state.userInfo && this.state.userInfo.name || this.state.name
     },
 
     render: function () {
@@ -40,13 +45,16 @@ var UserInfo = $class({
 
 var UserIcon = $class(UserInfo, {
     render: function () {
-        return this.state.isAnonymous? (
+        if(this.state.isAnonymous) return (
             <div className="user-icon user-icon-anon" style={{backgroundColor: this.state.color}}>
                 <i className={"icon glyphicon glyphicon-"+this.state.ico}></i>
             </div>
-        ):(
-            <div className="user-icon" style={{backgroundColor: this.state.color}}>
-                <Img src={this.state.icon} sizeLimit={50*1024} />
+        );
+        var icon = this.state.userInfo && this.state.userInfo.icon;
+        var name = this.state.name || "";
+        return (
+            <div className="user-icon" style={{backgroundColor: this.state.color}} title={name}>
+                {icon? <Img src={icon} sizeLimit={50*1024} /> : name && <span>{name[0]}</span> }
             </div>
         );
     }
